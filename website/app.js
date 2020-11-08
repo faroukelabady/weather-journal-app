b45f6ba5620c7d65df62b4153016459566320ccd
farouk <faroukelabady@hotmail.com>
2020-11-09 01:35:58 +0200

    Trying to modify the css a little

--- website/app.js
+++ website/app.js
@@ -4,6 +4,9 @@ const apiKey = '9ea801f0fb9f6cb0927948bc4ca20b43';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
 
 document.getElementById('generate').addEventListener('click', performAction);
 
@@ -12,11 +15,15 @@ function performAction(e) {
   const feeling = document.getElementById('feelings').value;
   getWeatherData(baseUrl, zipCode, apiKey).then( data => {
     console.log(data)
+    if(data.cod != '200') {
+      date.innerHTML = 'Could not get the weather info';
+      throw new Error('Could not get the weather info');
+    }
     postWeatherFeeling('/postData', {
       temperature : data.main.temp,
       date: newDate,
       feeling: feeling
-    });
+    });  
   }).then( recentEntry => {
     updateUI(recentEntry);
   })
@@ -25,12 +32,17 @@ function performAction(e) {
 }
 
 const getWeatherData = async (baseurl = '', zipCode,  apiId = apiKey) => {
+
+  date.innerHTML = 'Loading weather';
+  temp.innerHTML = '';
+  content.innerHTML = '';
   const url = baseurl + zipCode + '&units=metric&appid=' + apiId;
   const response = await fetch(url);
   try {
     const data = await response.json();
     return data;
   } catch (error) {
+    date.innerHTML = 'Could not get the weather info';
     console.log('error', error);
   }
 }
@@ -46,6 +58,8 @@ const postWeatherFeeling = async (url = '', data = {}) => {
   })
   try {
     const latestEntries = await response.json();
+    const date = document.getElementById('date');
+    date.innerHTML = '';
     return latestEntries; 
   } catch (error) {
     console.log('error', error);
@@ -54,14 +68,11 @@ const postWeatherFeeling = async (url = '', data = {}) => {
 
 const updateUI = async (recentEntries) => {
   const response = await fetch('/all');
-  const date = document.getElementById('date');
-  const temp = document.getElementById('temp');
-  const content = document.getElementById('content');
   try {
     const data = await response.json();
-    date.innerHTML = data.date;
-    temp.innerHTML = data.temperature + `&deg;C`;
-    content.innerHTML = data.feeling;
+    date.innerHTML = 'Current Date : ' + data.date;
+    temp.innerHTML = 'Temperature celsuis : ' + data.temperature + `&deg;C`;
+    content.innerHTML = 'How I feel : ' + data.feeling;
   } catch (error) {
     console.log('error', error);
   }
